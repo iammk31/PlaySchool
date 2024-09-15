fafd9ff7d065637ca202464560a535ac50ddb169
Mohit Kumar <80189839+iammk31@users.noreply.github.com>
2024-09-15 15:47:13 +0530

    created backend server and connected to database(mongo)

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { dbConnection } from './database/dbConnection.js';
+import { errorMiddleware } from './middleware/error.js';
+
+
+const app = express();
+dotenv.config({ path: './config/config.env' });
+app.use(express.json());
+
+
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+// Database connection
+dbConnection();
+
+// Error handling middleware
+app.use(errorMiddleware);
+
+export default app;
