c2c78ce7bf2ee3e9d9aeb2846c617d14083fb1fa
Mohit Kumar <80189839+iammk31@users.noreply.github.com>
2024-09-18 17:12:18 +0530

    updated news

--- client/src/Components/News.jsx
+++ client/src/Components/News.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { backendUrl } from '../../utils/config.js';
+import { backendUrl } from "../utils/config.js";
 
 const NewsCard = ({ imageUrl, information, timing }) => {
   return (
